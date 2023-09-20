835752af351a70543daf6f72f16c66a2eaa488a4
maheshdvp <maheshdvp9@gmail.com>
2023-09-20 19:34:32 +0530

    roboshop-shell-ansible

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,21 +1,25 @@
-data "aws_ami" "example" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "^myami-\\d{3}"
-  owners           = ["self"]
-
-  filter {
-    name   = "name"
-    values = ["myami-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+data "aws_ami_ids" "ami" {
+  name_regex = "centos-*"
+  owners = ["8328428660"]
 }
+#data "aws_ami" "example" {
+#  executable_users = ["self"]
+#  most_recent      = true
+#  name_regex       = "^myami-\\d{3}"
+#  owners           = ["self"]
+#
+#  filter {
+#    name   = "name"
+#    values = ["myami-*"]
+#  }
+#
+#  filter {
+#    name   = "root-device-type"
+#    values = ["ebs"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#}
