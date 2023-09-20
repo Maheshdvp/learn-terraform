486dde10752c1c204f4c48d386e8dfb1cef03d95
maheshdvp <maheshdvp9@gmail.com>
2023-09-20 16:35:28 +0530

    roboshop-shell-ansible

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -44,13 +44,13 @@ resource "aws_instance" "instance" {
 
 }
 
-resource "aws_route53_record" "records" {
+resource "aws_route53_record" "record" {
   for_each = var.components
   zone_id = "Z023814724XBO8OK2PSWK"
   name    = "${lookup(each.value,"name",null )}.devops999.store"
   type    = "A"
   ttl     = 30
-  records = [lookup(lookup(aws_instance,each.key,null),"private_ip",null)]
+  records = [lookup(lookup(aws_instance.instance,each.key,null),"private_ip",null)]
 }
 output "test" {
   value = lookup(var.components,"frontend","null" )
