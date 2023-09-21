97ad7e9bb2ad3f0fbbb71f276c91d41dc38993cb
maheshdvp <maheshdvp9@gmail.com>
2023-09-21 23:27:04 +0530

    roboshop-shell-ansible

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
 }
 
 resource "aws_route53_record" "record" {
