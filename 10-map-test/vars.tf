d9367da1aedff91ec4295e261facba0dad783ca1
maheshdvp <maheshdvp9@gmail.com>
2023-12-16 12:57:51 +0530

    roboshop-shell-ansible

--- 10-map-test/vars.tf
+++ 10-map-test/vars.tf
@@ -22,7 +22,7 @@ variable "vpc" {
 }
 
   output "all_subnets" {
-    value = {for k,v in merge(var.vpc["main"]["subnets"]):"subnets"=>v}
+    value = {for k,v in merge(var.vpc["main"]["subnets"]): k => v}
   }
 
 
